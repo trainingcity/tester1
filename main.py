4f14f29ac98d187a645b173eebf0de58614a7588
John Downing <john@trainingcity.com>
2023-12-18 14:08:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Read the example code below and write predictions what will happen
+#Reminder: To write a comment, first type a # and then type your text.
+# Predict and Run Example 1
+ # Add comments to the code below to explain what it does
+
+first_name = "Kevin"
+
+print(first_name)
+
+# Predict and Run Example 2
+ # Add comments to the code below to explain what it does
+
+last_name = "Gatera"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# Predict and Run Example 3
+ # Add comments to the code below to explain what it does
+
+print("Hello " + first_name + ". Your full name is " + full_name + ".")
